3dc26814614702eddd84d8d8f345650819df3023
UnaisPa <unaismuhmd14@gmail.com>
2024-12-19 00:01:56 +0530

    Implemented User Modal

---
+++ server/models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/\S+@\S+\.\S+/, 'is invalid']
+    },
+    password: {
+        type: String,
+    },
+    profilePicture: {
+        type: String,
+        default: ''
+    }
+
+}, {
+    timestamps: true  // Adds createdAt and updatedAt timestamps
+});
+
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = bcrypt.hash(this.password, salt);
+})
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password)
+}
+
+// Create the model from the schema and export it
+const User = mongoose.model('User', userSchema);
+
+export default User
