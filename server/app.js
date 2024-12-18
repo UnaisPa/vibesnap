a82749c475d1542de6fc5475ae4ff3ab8ea4cbcb
UnaisPa <unaismuhmd14@gmail.com>
2024-12-18 10:41:28 +0530

    .env configured

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import dotenv from "dotenv"
 const app = express();
-const port = 3000;
+
+dotenv.config();
+const port = process.env.PORT || 5000;
 
 // Define a route
 app.get('/', (req, res) => {
